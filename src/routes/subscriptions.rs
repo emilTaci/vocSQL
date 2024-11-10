a3a152554a1edf8b8293671fdb3867257c2f9606
emilTaci <emil.taciyev@gmail.com>
2024-11-10 23:53:51 +0400

    Add telemetry module and integrate tracing for improved logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,16 +10,12 @@ pub struct UserJSON {
     pub name: String,
 }
 
-pub async fn subscribe(user_json: web::Json<UserJSON>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = uuid::Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        user_json.email,
-        user_json.name
-    );
-
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(user_json, pool)
+)]
+pub async fn insert_subscriber(user_json: &UserJSON, pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
             INSERT INTO subscriptions (email, name, subscribed_at)
             VALUES ($1, $2, $3)
@@ -28,23 +24,26 @@ pub async fn subscribe(user_json: web::Json<UserJSON>, pool: web::Data<PgPool>) 
         user_json.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    .await
-    {
-        Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Created()
-                .json(create_response("success", "User subscribed successfully."))
-        }
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(user_json, pool),
+    fields(
+        subscriber_email = %user_json.email,
+        subscriber_name= %user_json.name
+    )
+)]
+pub async fn subscribe(user_json: web::Json<UserJSON>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&user_json, &pool).await {
+        Ok(_) => HttpResponse::Created()
+            .json(create_response("success", "User subscribed successfully.")),
         Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             if let Some(db_error) = e.as_database_error() {
                 if db_error.code().as_deref() == Some("23505") {
                     return HttpResponse::Conflict()
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,24 @@
+use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use voc_sql::configuration::get_configuration;
+use voc_sql::{
+    configuration::get_configuration,
+    telemetry::{get_subscriber, init_subscriber},
+};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 #[tokio::test]
 async fn health_check_works() {
@@ -18,11 +36,13 @@ async fn health_check_works() {
 }
 
 async fn spawn_app() -> String {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let pool = PgPool::connect(&configuration.database.connection_string())
+    let pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
