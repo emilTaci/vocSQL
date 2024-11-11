21bf17ab72341fa2b5c50f188a1794a187c88b80
emilTaci <emil.taciyev@gmail.com>
2024-11-11 13:15:34 +0400

    Update subscriber name and switch to lazy database connection

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,12 @@ use voc_sql::{
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("voc_sql".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
